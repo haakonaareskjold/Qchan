72aa0487756afa48cacb544ebf539ba48f13c0fc
haakonaareskjold <aareskjold.hhaakon@gmail.com>
2021-02-18 23:33:35 +0100

    removed imported css libs in master view for  (bootstrap and tailwind) , instead compiling through webpack (sass for bootstrap and postcss for tailwind)

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -16,12 +16,10 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
 </head>
 <body>
 <div id="app">
