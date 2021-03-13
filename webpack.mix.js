abe73bfd7a4572efffcce8ca46f233e22399a5f1
haakonaareskjold <aareskjold.hhaakon@gmail.com>
2021-03-13 19:24:22 +0100

    reverted gitlab-ci, updated laravel mix and scripts, updated webpack to remove sourcemaps and licence

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,12 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps()
     .postCss("resources/css/app.css", "public/css", [
         require("tailwindcss"),
     ])
+    .options({
+        terser: {
+            extractComments: false,
+        }
+    })
     .version();
