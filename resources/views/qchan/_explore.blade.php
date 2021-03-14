6c614f303ed39aeeeef059bfdd7a8b1eda06c94f
haakonaareskjold <aareskjold.hhaakon@gmail.com>
2021-03-14 16:59:25 +0100

    fixed explore logic to correctly not display followed users and self

--- resources/views/qchan/_explore.blade.php
+++ resources/views/qchan/_explore.blade.php
@@ -2,24 +2,17 @@
         <div class="flex flex-column relative">
             <div class="flex-wrap mb-4 relative">
             @foreach($users as $user)
-                    @foreach($following as $follows)
-                        @if($user->name === $follows->name)
-                            <div class="absolute bottom ml-6 text-red-500  font-bold text-right">User has already been followed</div>
+                        @if(!current_user()->following($user) && !current_user()->is($user))
+
+                                <a href="{{ $user->path() }}">
+                                    <img src="{{ $user->avatar }}"
+                                         alt="{{ $user->username }}'s avatar"
+                                         width="60px"
+                                         class="mr-4 rounded" />
+                                    <span class="font-bold d-block text-center mb-4">{{'@'.$user->username}}</span>
+                                </a>
                             @endif
                     @endforeach
-                @if(!current_user()->is($user))
-
-                <a href="{{ $user->path() }}">
-                    <img src="{{ $user->avatar }}"
-                         alt="{{ $user->username }}'s avatar"
-                         width="60px"
-                         class="mr-4 rounded" />
-                    <span class="font-bold d-block text-center mb-4">{{'@'.$user->username}}</span>
-                </a>
-                    @endif
-
-
-            @endforeach
                 @if($onlyUser)
                     <div class="text-lg font-bold text-red-500 mt-12">No other users exists at the moment, invite your friends!</div>
                     @endif
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: [
+      './resources/**/*.blade.php',
+      './resources/**/*.js'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
