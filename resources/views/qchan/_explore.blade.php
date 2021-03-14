2d42cb60edbbcf71cdfe0b2047194730463bcdba
haakonaareskjold <aareskjold.hhaakon@gmail.com>
2021-03-14 17:39:46 +0100

    recompiled assets with production, added header in explore for better UX/clarity

--- resources/views/qchan/_explore.blade.php
+++ resources/views/qchan/_explore.blade.php
@@ -1,15 +1,16 @@
 <x-App>
         <div class="flex flex-column relative">
             <div class="flex-wrap mb-4 relative">
+                <div class="font-weight-bold text-2xl mt-2 mb-6 border-bottom">Here you can explore new users!</div>
             @foreach($users as $user)
                         @if(!current_user()->following($user) && !current_user()->is($user))
 
-                                <a href="{{ $user->path() }}">
+                                <a href="{{ $user->path() }}" class="p-4">
                                     <img src="{{ $user->avatar }}"
                                          alt="{{ $user->username }}'s avatar"
                                          width="60px"
-                                         class="mr-4 rounded" />
-                                    <span class="font-bold d-block text-center mb-4">{{'@'.$user->username}}</span>
+                                         class="mr-4 rounded absolute" />
+                                    <span class="font-bold d-xl-block text-center mb-4">{{'@'.$user->username}}</span>
                                 </a>
                             @endif
                     @endforeach
