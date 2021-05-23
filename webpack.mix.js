e61f2b62c1b94cd52fa73e5c5f7760b91c164837
haakonaareskjold <aareskjold.haakon@gmail.com>
2021-05-23 19:47:53 +0200

    updated webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,6 +20,9 @@ mix.js('resources/js/app.js', 'public/js')
         terser: {
             extractComments: false,
         }
-    }).
-    browserSync('http://127.0.0.1:8000')
-    .version();
+    }).sourceMaps()
+    .browserSync('http://127.0.0.1:8000')
+
+if (mix.inProduction()) {
+    mix.version();
+}
