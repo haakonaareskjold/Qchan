e255a552e22a1f9a4bfa06b96bb224aa3d3097b0
haakonaareskjold <aareskjold.haakon@gmail.com>
2021-10-13 17:12:29 +0200

    Feat(ide_helper): add ide helper package, added quotes on ports for docker-compose.yml , updated gitignore

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,25 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * App\Models\Post
- *
- * @mixin Builder
- * @property int $id
- * @property int $user_id
- * @property string $body
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \App\Models\User $user
- * @method static \Illuminate\Database\Eloquent\Builder|Post newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Post newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Post query()
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereBody($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Post whereUserId($value)
- */
+
 class Post extends Model
 {
     use HasFactory;
